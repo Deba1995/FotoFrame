984e696e0bfe015717ca0200aa35de30bcf3c549
root <deba@admin.com>
2024-01-14 23:14:52 +0530

    compiled assets

---
+++ FOTO_PRAME_CLIENT/fotoprame/app/West.js
@@ -0,0 +1,2 @@
+import{r,i as t}from"./Home.js";import{j as a}from"./index.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var o=e.default=void 0,i=u(r()),s=a,l=(0,i.default)((0,s.jsx)("path",{d:"m9 19 1.41-1.41L5.83 13H22v-2H5.83l4.59-4.59L9 5l-7 7 7 7z"}),"West");o=e.default=l;export{o as d};
+//# sourceMappingURL=West.js.map
--- FOTO_PRAME_CLIENT/vite.config.js
+++ FOTO_PRAME_CLIENT/vite.config.js
@@ -1,60 +1,19 @@
-// vite.config.js
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-import { dependencies } from "./package.json";
-
-function renderChunks(deps) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, "env");
-
-  return {
-    server: { hmr: true },
-    plugins: [
-      react({
-        include: ["**/*.jsx", "**/*.js"],
-      }),
-    ],
-    resolve: {
-      alias: { "@": path.resolve(__dirname, "src/") },
-    },
-    css: {
-      postcss: (ctx) => ({
-        parser: ctx.parser ? "sugarss" : false,
-        map: ctx.env === "development" ? ctx.map : false,
-        plugins: {
-          "postcss-import": {},
-          "postcss-nested": {},
-          cssnano: ctx.env === "production" ? {} : false,
-          autoprefixer: { overrideBrowserslist: ["defaults"] },
-        },
-      }),
-    },
-    build: {
-      sourcemap: false,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ["react", "react-router-dom", "react-dom"],
-            ...renderChunks(dependencies),
-          },
-        },
+export default defineConfig({
+  base: "/", // Update the base URL to match your deployed website
+  plugins: [react()],
+  build: {
+    outDir: "./fotoprame/app/",
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
       },
     },
-    test: {
-      globals: true,
-      coverage: {
-        reporter: ["text", "json", "html"],
-      },
-    },
-  };
+  },
 });
