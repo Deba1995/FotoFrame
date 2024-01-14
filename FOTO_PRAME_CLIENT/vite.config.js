c77e1478e3cd4c50f01e7d6579c87a3ba3c2d253
root <deba@admin.com>
2024-01-14 21:35:26 +0530

    vite.config chnages

--- FOTO_PRAME_CLIENT/vite.config.js
+++ FOTO_PRAME_CLIENT/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/foto-frame/", // Update the base URL to match your deployed website
   plugins: [react()],
+  build: {
+    // Set to 'production' for production optimizations
+    mode: "production",
+    // Enable minification
+    minify: "terser",
+    // Configure code splitting
+    chunkSizeWarningLimit: 2000,
+    // Enable tree shaking
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    // Enable cache busting
+    outDir: "dist",
+    assetsDir: "assets",
+    assetsInlineLimit: 4096,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
+  },
 });
